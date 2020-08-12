57303f69d2cb84a8245a8be9e68d979b4ed6646c
Andrey Mulenkov <amulenkov@wanna.by>
2020-08-12 18:45:03 +0300

    added model implementation with tests

---
+++ source/model/supernet.py
@@ -0,0 +1,242 @@
+import torch
+import torch.nn
+import itertools
+
+import torch.functional as F
+from dataclasses import dataclass
+from typing import Iterable, List, Dict, Tuple, Set, Optional
+
+@dataclass
+class SupernetBlockSpec:
+    """Supernet block specification
+    """
+    name: str
+    kind: str
+    params: dict
+
+    @classmethod
+    def from_dict(spec: Dict) -> 'SupernetBlockSpec':
+        return SupernetBlockSpec(**spec)
+
+@dataclass
+class SupernetLayerSpec:
+    """Supernet layer specification
+    """
+    blocks: List[SupernetBlockSpec]
+
+    @classmethod
+    def from_dict(spec: Dict) -> 'SupernetLayerSpec':
+        layer_params = {k: v for k, v in spec.items() if k != 'blocks'}
+        return SupernetLayerSpec(
+            blocks=[SupernetBlockSpec.from_dict({**layer_params, **params}) for params in spec['blocks']]
+        )
+
+@dataclass
+class SupernetClassifierSpec:
+    """Supernet model config
+    """
+    input_shape: Tuple[int, int]
+    input_channels: int
+    output_layer_channels: int
+    num_classes: int
+    supernet_layers_spec: List[SupernetLayerSpec]\
+
+    @classmethod
+    def from_dict(spec: Dict) -> 'SupernetClassifierSpec':
+        return SupernetClassifierSpec(
+            input_shape=spec['input_shape'],
+            input_channels=spec['input_channels'],
+            output_layer_channels=spec['output_layer_channels'],
+            num_classes=spec['num_classes'],
+            supernet_layers_spec=[SupernetLayerSpec.from_dict(x) for x in spec['supernet_layers_spec']]
+        )
+
+
+def get_output_size(module: torch.nn.Module, input_size: torch.Size, **params) -> torch.Size:
+    """ Compute output size for input of given size
+        For Conv2d this can be computed analytically, but from maintanence and generalization standpoint I much prefer brute-force method
+    """
+    in_tensor = torch.zeros(input_size)
+    out_tensor = module(in_tensor, **params)
+    return out_tensor.size()
+
+
+def build_block(block_spec: SupernetBlockSpec, in_channels: int, out_channels: int) -> torch.nn.Module:
+    """Build a block according to specification.
+       This is a simple extension point for future experiments, albeit not necessary for given task
+    """
+
+    if block_spec.kind == 'conv2d':
+        return torch.nn.Conv2d(in_channels=in_channels, 
+                               out_channels=out_channels, 
+                               **block_spec.params)
+
+    if block_spec.kind == 'identity':
+        return torch.nn.Identity()
+    
+    raise ValueError(f'Unknown block kind: {block_spec.kind}') 
+
+
+class PadToWidth(torch.nn.Module):
+
+    def __init__(self, embedding_size: int, fill_value: float = 0):
+        super(PadToWidth, self).__init__()
+        self._embedding_size = embedding_size
+        self._fill_value = fill_value
+    
+    def forward(self, x):
+        input_size = x.size()
+        target_size = torch.Size((*input_size[:-1], self._embedding_size))
+        result = torch.full(target_size, dtype=x.dtype, fill_value=self._fill_value)
+        result[..., :input_size[-1]] = x
+        return result
+
+
+class SupernetLayer(torch.nn.Module):
+
+    def __init__(self,
+                 blocks: Dict[str, torch.nn.Module],
+                 activation: torch.nn.Module = torch.nn.ReLU(),
+                 pooling: torch.nn.Module = torch.nn.MaxPool2d(kernel_size=2),
+                 default_block: Optional[str] = None):
+
+        """Supernet cell.
+           Implements a NN layer with switchable inner block
+           Active block can be selected by calling .select_block
+
+        Args:
+            blocks (Dict[str, torch.nn.Module]): inner blocks. 
+                                                 Only one is active at any time, although this behaiviour could be changed by paddnig and summing the outputs
+            activation (torch.nn.Module, optional): Activation function. Defaults to torch.nn.ReLU.
+            pooling (torch.nn.Module, optional): Pooling operation. Defaults to torch.nn.MaxPool2d with 2x2 kernel
+            default_block (Optional[str], optional): Name of the default block. Defaults to None
+
+        Raises:
+            ValueError: [description]
+        """
+
+        if len(blocks) == 0:
+            raise ValueError("At least one block should be passed")
+
+        super(SupernetLayer, self).__init__()
+
+        self._blocks = torch.nn.ModuleDict(blocks) 
+        self._active_block = None
+        self._activation = activation
+        self._pooling = pooling
+
+        self.select_block(default_block)
+
+    @property
+    def available_blocks(self) -> Set[str]:
+        """Set of available layer block names
+        """
+        return set(self._blocks.keys())
+
+    def get_output_sizes(self, input_size: torch.Size) -> Dict[str, torch.Size]:
+        """Get dictionary with output sizes for given input size
+        """
+        return {
+            name: get_output_size(self, input_size, with_block=name)
+            for name in self.available_blocks
+        }
+
+    def select_block(self, block_name: Optional[str]):
+        """Select a block with given name as active
+        """
+
+        if block_name is None:
+            self._active_block = None
+        elif block_name in self.available_blocks:
+            self._active_block = block_name
+        else:
+            raise ValueError(f'Block with name {block_name} does not exist')
+
+    def distill(self):
+        """Extract torch.nn.Sequential module equivalent to current layer configuration
+        """
+        return torch.nn.Sequential(
+            self._blocks[self._active_block],
+            self._activation,
+            self._pooling
+        )
+
+    def forward(self, x: torch.Tensor, with_block: Optional[str] = None) -> torch.Tensor:
+        
+        if self._active_block is None and with_block is None:
+            raise ValueError("One of the block should be selected as active")
+
+        selected_block = self._blocks[self._active_block] if with_block is None else self._blocks[with_block]
+        x = selected_block.forward(x)
+        x = self._activation(x)
+        x = self._pooling(x)
+
+        return x
+
+
+class SupernetClassifier(torch.nn.Module):
+
+    def __init__(self, 
+                 input_size: Tuple[int, int],
+                 num_input_channels: int, 
+                 num_output_layer_channels: int,
+                 num_classes: int,
+                 supernet_layers: List[SupernetLayer]):
+
+        """Supernet classifier model
+
+        Args:
+            input_size (Tuple[int, int]): input resulotion
+            num_input_channels (int): number of channels in input
+            num_classes (int): number of classes
+            supernet_layers (List[SupernetLayer]): list of supernet layers
+        """
+        super(SupernetClassifier, self).__init__()
+
+        input_size = torch.Size((1, num_input_channels, *input_size))
+        possible_output_sizes = {input_size}
+        for layer in supernet_layers:
+            possible_output_sizes = set().union(*(
+                set(layer.get_output_sizes(s).values())
+                for s in possible_output_sizes
+            ))
+      
+        self._input_size = input_size
+        self._supernet_layers = torch.nn.ModuleList(supernet_layers)
+        self._embedding_size = max(s.numel() for s in possible_output_sizes)
+        self._output_layers = torch.nn.Sequential(
+            torch.nn.Flatten(start_dim=1),
+            PadToWidth(self._embedding_size),
+            torch.nn.Linear(in_features=self._embedding_size, out_features=num_output_layer_channels),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=num_output_layer_channels, out_features=num_classes),
+            torch.nn.Softmax()
+        )
+
+    def reconfigure(self, block_names: Iterable[str]):
+
+        if len(block_names) != len(self._supernet_layers):
+            raise ValueError(f'Model contains {len(self._supernet_layers)} supernet layers, but passed {len(block_names)} block names')
+
+        for i, name in enumerate(block_names):
+
+            layer = self._supernet_layers[i]
+            if name not in layer.available_blocks:
+                raise ValueError(f'Layer does not contain block {name}. Available blocks: {layer.available_block}')
+
+            layer.select_block(name)
+
+    def distill(self):
+
+        supernet_layers = [layer.distill() for layer in self._supernet_layers]
+        return torch.nn.Sequential(
+            *supernet_layers,
+            self._output_layers
+        )
+
+    def forward(self, x: torch.Tensor):
+
+        for layer in self._supernet_layers:
+            x = layer(x)
+
+        return self._output_layers(x)
---
+++ source/model/test_supernet.py
@@ -0,0 +1,123 @@
+import pytest
+import torch
+import itertools
+from .supernet import SupernetLayer, SupernetClassifier
+
+
+def test_supernet_layer_forward():
+
+    layer = SupernetLayer({
+        'conv3x3': torch.nn.Conv2d(in_channels=4, out_channels=8, kernel_size=(3, 3)),
+        'conv5x5': torch.nn.Conv2d(in_channels=4, out_channels=8, kernel_size=(5, 5)),
+        'conv7x7': torch.nn.Conv2d(in_channels=4, out_channels=8, kernel_size=(7, 7))
+    })
+
+    test_data = torch.rand(1, 4, 8, 8)
+
+    with pytest.raises(ValueError):
+        results = layer(test_data)
+
+    layer.select_block('conv3x3')
+    results3x3 = layer(test_data)
+
+    layer.select_block('conv5x5')
+    results5x5 = layer(test_data)
+
+    layer.select_block('conv7x7')
+    results7x7 = layer(test_data)
+
+    assert results3x3.size != results5x5.size or not torch.allclose(results3x3, results5x5)
+    assert results3x3.size != results7x7.size or not torch.allclose(results3x3, results7x7) 
+    assert results5x5.size != results7x7.size or not torch.allclose(results5x5, results7x7)
+
+def test_supernet_layer_distill():
+
+    layer = SupernetLayer({
+        'conv3x3': torch.nn.Conv2d(in_channels=4, out_channels=8, kernel_size=(3, 3)),
+        'conv5x5': torch.nn.Conv2d(in_channels=4, out_channels=8, kernel_size=(5, 5)),
+        'conv7x7': torch.nn.Conv2d(in_channels=4, out_channels=8, kernel_size=(7, 7))
+    })
+
+    test_data = torch.rand(1, 4, 8, 8)
+
+    layer.select_block('conv5x5')
+    distilled_layer = layer.distill()
+
+    assert torch.allclose(layer(test_data), distilled_layer(test_data))
+
+@pytest.mark.parametrize('batch_size, in_channels, out_channels, width, height', [
+    (6, 8, 16, 24, 32),
+    (1, 8, 16, 24, 32),
+    (6, 1, 4, 8, 8)
+])
+def test_supernet_layer_sizes(batch_size, in_channels, out_channels, width, height):
+
+    layer = SupernetLayer({
+        'conv3x3': torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(3, 3)),
+        'conv5x5': torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(5, 5)),
+        'conv7x7': torch.nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(7, 7))
+    })
+
+    input_size = torch.Size((batch_size, in_channels, width, height))
+    output_sizes = layer.get_output_sizes(input_size)
+    expected_sizes = {
+        'conv3x3': torch.Size((batch_size, out_channels, (width - 2) // 2, (height - 2) // 2)),
+        'conv5x5': torch.Size((batch_size, out_channels, (width - 4) // 2, (height - 4) // 2)),
+        'conv7x7': torch.Size((batch_size, out_channels, (width - 6) // 2, (height - 6) // 2))
+    }
+    
+    assert output_sizes == expected_sizes
+
+def test_supernet_model_forward():
+
+    model = SupernetClassifier(input_size=(32, 32),
+                               num_input_channels=1,
+                               num_output_layer_channels=64,
+                               num_classes=2, 
+                               supernet_layers=[
+        SupernetLayer({
+            'conv3x3': torch.nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(3, 3)),
+            'conv5x5': torch.nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(5, 5))
+        }),
+        SupernetLayer({
+            'conv3x3': torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3)),
+            'conv5x5': torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(5, 5))
+        })
+    ])
+
+    test_data = torch.rand(4, 1, 32, 32)
+    results = {}
+
+    for configuration in itertools.combinations(['conv3x3', 'conv5x5'], 2):
+        model.reconfigure(configuration)
+        results[tuple(configuration)] = model(test_data)
+
+    for c1, c2 in itertools.combinations(results.keys(), 2):
+        if c1 != c2:
+            assert not torch.allclose(results[c1], results[c2])
+
+def test_supernet_model_distill():
+
+    model = SupernetClassifier(input_size=(32, 32),
+                               num_input_channels=1,
+                               num_output_layer_channels=64,
+                               num_classes=2, 
+                               supernet_layers=[
+        SupernetLayer({
+            'conv3x3': torch.nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(3, 3)),
+            'conv5x5': torch.nn.Conv2d(in_channels=1, out_channels=16, kernel_size=(5, 5))
+        }),
+        SupernetLayer({
+            'conv3x3': torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3)),
+            'conv5x5': torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(5, 5))
+        })
+    ])
+
+    for configuration in itertools.combinations(['conv3x3', 'conv5x5'], 2):
+        model.reconfigure(configuration)
+        distilled_model = model.distill()
+
+        test_data = torch.rand(4, 1, 32, 32)
+        full_model_output = model(test_data)
+        distilled_model_output = distilled_model(test_data)
+        assert torch.allclose(full_model_output, distilled_model_output)
